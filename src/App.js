7edec2802872d5e116483576ac163aea04f6971b
liana5555 <baloghildiko55@gmail.com>
2023-01-21 19:13:10 +0100

    feat: Some clicking functionality but it will be updated in the future

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import down from './arrow-down-solid.svg';
 import up from './arrow-up-solid.svg';
 import './App.css';
+import React from 'react';
 
-
+//LETS CREATE COMPONENTS AND TRY TO GET EVERYTHING WITH JUST ONE STATE
 
 function App() {
+
+  const [breakLength, setBreakLength] = React.useState(5)
+  const [sessionLength, setSessionLength] = React.useState(25)
+
+  function handleReset () {
+
+    setBreakLength(5)
+    setSessionLength(25)
+
+  }
+
+  function handleSettingBreakOrSessionLength(event){
+    const increase_or_decrease = event.target.parentNode.id
+     if (increase_or_decrease === "break-decrement") {
+      setBreakLength(prev=> prev-1)
+    }
+    else if (increase_or_decrease === "break-increment") {
+      setBreakLength(prev=> prev+1)
+    }
+    else if(increase_or_decrease === "session-decrement") {
+      setSessionLength(prev => prev-1)
+    }
+    else if (increase_or_decrease === "session-increment") {
+      setSessionLength(prev => prev+1)
+    }
+
+   console.log(increase_or_decrease)
+
+  }
 
   return (
     <main>
@@ -13,18 +43,18 @@ function App() {
         <div className='break_container'>
           <div id='break-label'>Break Length</div>
           <div className='break_counter_container'>
-            <div id="break-decrement"><img src={down} alt="decrement break length"/></div>
-            <div id="break-length">5</div>
-            <div id="break-increment"><img src={up} alt="increment break length"/></div>
+            <div id="break-decrement" onClick={handleSettingBreakOrSessionLength}><img src={down} alt="decrement break length"/></div>
+            <div id="break-length">{breakLength}</div>
+            <div id="break-increment" onClick={handleSettingBreakOrSessionLength}><img src={up} alt="increment break length"/></div>
 
           </div>
         </div>
         <div className='session_length_container'>
           <div id='session-label'>Session Length</div>
           <div className='session_counter_container'>
-            <div id="session-decrement"><img src={down} alt="decrement break length"/></div>
-            <div id="session-length">25</div>
-            <div id="session-increment"><img src={up} alt="increment break length"/></div>
+            <div id="session-decrement" onClick={handleSettingBreakOrSessionLength}><img src={down} alt="decrement break length"/></div>
+            <div id="session-length">{sessionLength}</div>
+            <div id="session-increment" onClick={handleSettingBreakOrSessionLength}><img src={up} alt="increment break length"/></div>
 
           </div>
         </div>
@@ -36,7 +66,7 @@ function App() {
           <div id="timer-left">25:00</div>
           <div className='button_container'>
             <div id="start_stop">start/stop</div>
-            <div id="reset">Reset</div>
+            <div id="reset" onClick={handleReset}>Reset</div>
           </div>
       </div>
       </main>
